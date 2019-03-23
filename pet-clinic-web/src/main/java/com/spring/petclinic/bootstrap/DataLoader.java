05cf833fa90071f1864c7a232808c97437d2dd38
rodrigo.magalhaes <rodrigo.magalhaes@projuris.com.br>
2019-03-23 11:43:55 -0300

    added PetType to bootstrap

--- pet-clinic-web/src/main/java/com/spring/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/spring/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.spring.petclinic.bootstrap;
 
 import com.spring.petclinic.model.Owner;
+import com.spring.petclinic.model.PetType;
 import com.spring.petclinic.model.Vet;
 import com.spring.petclinic.services.OwnerService;
+import com.spring.petclinic.services.PetTypeService;
 import com.spring.petclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,15 +15,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDog = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCat = petTypeService.save(cat);
 
         Owner peleh = new Owner("Pelé", "O Rei");
         Owner garrincha = new Owner("Ronaldo", "Fenomeno");
