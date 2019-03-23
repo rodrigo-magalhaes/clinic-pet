d7059e9a6e88ee382cc349210d097f58f1a149cb
rodrigo.magalhaes <rodrigo.magalhaes@projuris.com.br>
2019-03-23 14:06:38 -0300

    fixed broken links

--- pet-clinic-web/src/main/java/com/spring/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/spring/petclinic/controllers/OwnerController.java
@@ -24,4 +24,10 @@ public class OwnerController {
 
         return "owners/index";
     }
+
+    @RequestMapping({"/find", "/find.html"})
+    public String findOwners() {
+
+        return "notimplemented";
+    }
 }
