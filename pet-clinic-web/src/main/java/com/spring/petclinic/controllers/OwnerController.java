6598c90ea8008a685a440e81113c1c45c3352ca0
rodrigo.magalhaes <rodrigo.magalhaes@projuris.com.br>
2019-05-07 18:39:50 -0300

    Added create or update owner.

--- pet-clinic-web/src/main/java/com/spring/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/spring/petclinic/controllers/OwnerController.java
@@ -8,14 +8,22 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+
+    public static final String VIEWS_OWNER_CREATE_OR_UPDATE = "owners/createOrUpdateOwnerForm";
+    public static final String VIEWS_OWNER_LIST = "owners/ownersList";
+    public static final String VIEWS_OWNER_REDIRECT = "redirect:/owners/";
+    public static final String VIEWS_OWNER_FIND = "owners/findOwners";
+    public static final String VIEWS_OWNER_DETAILS = "owners/ownerDetails";
 
     private final OwnerService ownerService;
 
@@ -24,18 +32,10 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-//    @RequestMapping({"", "/", "/index", "/index.html"})
-//    public String listOwners(Model model) {
-//
-//        model.addAttribute("owners", ownerService.findAll());
-//
-//        return "owners/index";
-//    }
-
     @RequestMapping({"/find", "/find.html"})
     public String findOwners(Model model) {
         model.addAttribute("owner", Owner.builder().build());
-        return "owners/findOwners";
+        return VIEWS_OWNER_FIND;
     }
 
     @GetMapping
@@ -49,20 +49,54 @@ public class OwnerController {
         if(owners.isEmpty()) {
             //no owners found
             result.rejectValue("LastName", "notFound", "not found");
-            return "owners/findOwners";
+            return VIEWS_OWNER_FIND;
         } else if(owners.size() == 1) {
             owner = owners.iterator().next();
-            return "redirect:/owners/"+owner.getId();
+            return VIEWS_OWNER_REDIRECT+owner.getId();
         } else {
             model.addAttribute("selections", owners);
-            return "owners/ownersList";
+            return VIEWS_OWNER_LIST;
         }
     }
 
     @GetMapping("/{ownerId}")
     public ModelAndView showOwner(@PathVariable("ownerId") int ownerId) {
-        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        ModelAndView mav = new ModelAndView(VIEWS_OWNER_DETAILS);
         mav.addObject("owner", this.ownerService.findById(new Long(ownerId)));
         return mav;
+    }
+
+    @GetMapping("/new")
+    public String initCreationForm(Model model) {
+        model.addAttribute("owner", Owner.builder().build());
+        return VIEWS_OWNER_CREATE_OR_UPDATE;
+    }
+
+    @PostMapping("/new")
+    public String processCreationForm(@Valid Owner owner, BindingResult result) {
+        if (result.hasErrors()) {
+            return VIEWS_OWNER_CREATE_OR_UPDATE;
+        } else {
+            Owner savedOwner = ownerService.save(owner);
+            return VIEWS_OWNER_REDIRECT+savedOwner.getId();
+        }
+    }
+
+    @GetMapping("/{ownerId}/edit")
+    public String initUpdateForm(@PathVariable("ownerId") Long ownerId, Model model) {
+        model.addAttribute(ownerService.findById(ownerId));
+        return VIEWS_OWNER_CREATE_OR_UPDATE;
+    }
+
+    @PostMapping("/{ownerId}/edit")
+    public String processUpdateForm(@Valid Owner owner, BindingResult result,
+                                    @PathVariable("ownerId") Long ownerId) {
+        if (result.hasErrors()) {
+            return VIEWS_OWNER_CREATE_OR_UPDATE;
+        } else {
+            owner.setId(ownerId);
+            ownerService.save(owner);
+            return VIEWS_OWNER_REDIRECT+ownerId;
+        }
     }
 }
--- pet-clinic-web/src/test/java/com/spring/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/spring/petclinic/controllers/OwnerControllerTest.java
@@ -16,9 +16,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -56,6 +58,7 @@ class OwnerControllerTest {
     void processFindFormReturnMany() throws Exception {
         when(ownerService.finadAllByLastNameLike(anyString()))
                 .thenReturn(Arrays.asList(Owner.builder().id(1L).build(), Owner.builder().id(2L).build()));
+
         mockMvc.perform(get("/owners"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/ownersList"))
@@ -69,5 +72,49 @@ class OwnerControllerTest {
         mockMvc.perform(get("/owners"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/owners/1"));
+    }
+
+    @Test
+    void initCreationForm() throws Exception {
+        mockMvc.perform(get("/owners/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/createOrUpdateOwnerForm"))
+                .andExpect(model().attributeExists("owner"));
+
+        verifyZeroInteractions(ownerService);
+    }
+
+    @Test
+    void processCreationForm() throws Exception {
+        when(ownerService.save(any())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(post("/owners/new"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"))
+                .andExpect(model().attributeExists("owner"));
+
+        verify(ownerService).save(any());
+    }
+
+    @Test
+    void initUpdateForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/1/edit"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/createOrUpdateOwnerForm"))
+                .andExpect(model().attributeExists("owner"));
+
+        verifyZeroInteractions(ownerService);
+    }
+
+    @Test
+    void processUpdateForm() throws Exception {
+        mockMvc.perform(post("/owners/1/edit"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"))
+                .andExpect(model().attributeExists("owner"));
+
+        verify(ownerService).save(any());
     }
 }
